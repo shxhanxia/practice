36c785367e4c0119f12271c31f2afacd35d378ff
韩侠公司 <hanxia@hanxia.work>
2018-04-12 16:10:47 +0800

    x

--- node/ergodic.js
+++ node/ergodic.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 //解析需要遍历的文件夹，我这以F盘为例
-var filePath = path.resolve('F:/WWW');
+var filePath = path.resolve('F:/WWW/practice');
 
 //调用文件遍历方法
 fileDisplay(filePath);
